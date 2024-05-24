0a01a0a9b0c4a6438d508127e96ba706e2d24ac4
Sara Fletcher <saflebri@gmail.com>
2024-05-24 10:55:20 -0500

    Adding GZIP - compression algorithm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const compression = require("express-compression");
 const cookieParser = require("cookie-parser");
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
+app.use(compression());
 
 // Using passport for authentication
 initializePassport();
